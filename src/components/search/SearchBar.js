ce3befa49165123a17f05488d1e206503f4e62c3
muideen7 <olayeyeayomide2000@gmail.com>
2023-05-30 22:13:19 +0100

    SearchBar.js

---
+++ src/components/search/SearchBar.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './SearchBar.css';
+
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <form className="search-bar" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter location"
+        value={query}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
+
