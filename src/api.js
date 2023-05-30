d0b8fcca172139473ab5cee590fdb47723ac291b
muideen7 <olayeyeayomide2000@gmail.com>
2023-05-30 23:03:11 +0100

    api.js

--- src/api.js
+++ src/api.js
@@ -9,5 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "6200fa753e0c0dfad812f704ad34b53a";
-
+export const WEATHER_API_KEY = "6200fa753e0c0dfad812f704ad34b53a":
