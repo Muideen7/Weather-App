1b45d299c5aa9c29c8c4caf0979c1fa6c36db3de
muideen7 <olayeyeayomide2000@gmail.com>
2023-05-31 22:49:59 +0100

    weather-app

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "a12a030438msh1badaae85621a90p162105jsn9125aeb8a4f2",
+    "X-RapidAPI-Key": "Your_geodb_api",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "6200fa753e0c0dfad812f704ad34b53a":
+export const WEATHER_API_KEY = "Your_Open_Weather_api":
