7124fbd74112283c4c906b1027e62bf9d27ea43e
muideen7 <olayeyeayomide2000@gmail.com>
2023-06-04 13:35:45 +0100

    webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+iconst path = require('path');
+
+module.exports = {
+  // ...
+  resolve: {
+    fallback: {
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      crypto: require.resolve('crypto-browserify'),
+    },
+  },
+  // ...
+};
